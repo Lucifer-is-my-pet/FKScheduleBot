683404692b3ce6536f2c7fa7677bfe81bd97b3a5
Дарья Гуральник <6801611+Lucifer-is-my-pet@users.noreply.github.com>
2019-10-30 17:37:51 +0500

    расписание на ЗФБ-20

--- bot.py
+++ bot.py
@@ -6,51 +6,51 @@ import os
 
 bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
 
-TODAY = date.today()  # date(2019, 1, 28)
+TODAY = date.today()  # date(2020, 1, 28)
 battle_kind = "wtf"  # fb
 
 QUEST_SCHEDULE = {
-    "визитка": [date(2019, 1, 19), date(2019, 1, 20)],
-    "тексты нерейтинг": [date(2019, 1, 26), date(2019, 1, 27)],
-    # "драбблы нерейтинг": [date(2019, 1, 26), date(2019, 1, 27)],
+    "визитка": [date(2020, 1, 18), date(2020, 1, 19)],
+    "тексты нерейтинг": [date(2020, 1, 25), date(2020, 1, 26)],
+    # "драбблы нерейтинг": [date(2020, 1, 26), date(2020, 1, 27)],
     # "мини нерейтинг": [date(TODAY.year, 1, 26), date(TODAY.year, 1, 27)],
-    "визуал нерейтинг": [date(2019, 2, 1), date(2019, 2, 2)],
+    "визуал нерейтинг": [date(2020, 1, 31), date(2020, 2, 1)],
     # "миди нерейтинг": [date(TODAY.year, 1, 26), date(TODAY.year, 1, 27)],
-    "челлендж": [date(2019, 2, 8), date(2019, 2, 9)],
+    "челлендж": [date(2020, 2, 7), date(2020, 2, 8)],
     # "бб": [date(TODAY.year, 8, 26), date(TODAY.year, 8, 27)],
-    "тексты рейтинг": [date(2019, 2, 16), date(2019, 2, 17)],
-    # "драбблы рейтинг": [date(2019, 2, 16), date(2019, 2, 17)],
+    "тексты рейтинг": [date(2020, 2, 15), date(2020, 2, 16)],
+    # "драбблы рейтинг": [date(2020, 2, 16), date(2020, 2, 17)],
     # "мини рейтинг": [date(TODAY.year, 2, 16), date(TODAY.year, 2, 17)],
-    "визуал рейтинг": [date(2019, 2, 22), date(2019, 2, 23)],
+    "визуал рейтинг": [date(2020, 2, 21), date(2020, 2, 22)],
     # "миди рейтинг": [date(TODAY.year, 2, 16), date(TODAY.year, 2, 17)],
-    "спецквест": [date(2019, 2, 28), date(2019, 3, 1)]}
+    "спецквест": [date(2020, 2, 27), date(2020, 2, 28)]}
 
 VOTE_SCHEDULE = [
-    {"name": "визитка", "beginning": date(2019, 1, 21), "end": date(2019, 2, 3),
+    {"name": "визитка", "beginning": date(2020, 1, 20), "end": date(2020, 2, 2),
      "out": "визитку"},
-    {"name": "тексты нерейтинг", "beginning": date(2019, 1, 28), "end": date(2019, 2, 10),
+    {"name": "тексты нерейтинг", "beginning": date(2020, 1, 27), "end": date(2020, 2, 9),
      "out": "нерейтинговые тексты", "rating": False},
-    # {"name": "драбблы нерейтинг", "beginning": date(2019, 1, 28), "end": date(2019, 2, 10),
+    # {"name": "драбблы нерейтинг", "beginning": date(2020, 1, 28), "end": date(2020, 2, 10),
     #  "out": "нерейтинговые драбблы", "rating": False},
     # {"name": "мини нерейтинг", "beginning": date(TODAY.year, 8, 2), "end": date(TODAY.year, 8, 23),
     # "out": "нерейтинговые мини", "rating": False},
-    {"name": "визуал нерейтинг", "beginning": date(2019, 2, 3), "end": date(2019, 2, 16),
+    {"name": "визуал нерейтинг", "beginning": date(2020, 2, 2), "end": date(2020, 2, 15),
      "out": "нерейтинговый визуал", "rating": False},
     # {"name": "миди нерейтинг", "beginning": date(TODAY.year, 8, 12), "end": date(TODAY.year, 9, 2),
     # "out": "нерейтинговые миди", "rating": False},
-    {"name": "челлендж", "beginning": date(2019, 2, 10), "end": date(2019, 2, 23), "out": "челлендж"},
+    {"name": "челлендж", "beginning": date(2020, 2, 9), "end": date(2020, 2, 22), "out": "челлендж"},
     # {"name": "бб", "beginning": date(TODAY.year, 8, 28), "end": date(TODAY.year, 10, 19), "out": "ББ"},
-    {"name": "тексты рейтинг", "beginning": date(2019, 2, 18), "end": date(2019, 3, 3),
+    {"name": "тексты рейтинг", "beginning": date(2020, 2, 17), "end": date(2020, 3, 2),
      "out": "нерейтинговые тексты", "rating": True},
-    # {"name": "драбблы рейтинг", "beginning": date(2019, 9, 8), "end": date(2019, 9, 29),
+    # {"name": "драбблы рейтинг", "beginning": date(2020, 9, 8), "end": date(2020, 9, 29),
     #  "out": "рейтинговые драбблы", "rating": True},
     # {"name": "мини рейтинг", "beginning": date(TODAY.year, 9, 13), "end": date(TODAY.year, 10, 4),
     # "out": "рейтинговые мини", "rating": True},
-    {"name": "визуал рейтинг", "beginning": date(2019, 2, 24), "end": date(2019, 3, 9),
+    {"name": "визуал рейтинг", "beginning": date(2020, 2, 23), "end": date(2020, 3, 7),
      "out": "рейтинговый визуал", "rating": True},
     # {"name": "миди рейтинг", "beginning": date(TODAY.year, 9, 23), "end": date(TODAY.year, 10, 14),
     # "out": "рейтинговые миди", "rating": True},
-    {"name": "спецквест", "beginning": date(2019, 3, 2), "end": date(2019, 3, 15), "out": "спецквест"}]
+    {"name": "спецквест", "beginning": date(2020, 2, 29), "end": date(2020, 3, 13), "out": "спецквест"}]
 
 
 def is_not_in_quest_list(word):
